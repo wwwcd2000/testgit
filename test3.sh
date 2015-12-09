f7ff69dc0720a03d0db2a315ac09d6da4e1d48bc
wwwcd2000 <1036281315@qq.com>
2015-12-09 18:45:14 +0800

    add test3.sh

---
+++ test3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ $# -lt 3 ];then
+ 
+echo "只接受三个数字参数"
+exit 3
+elif [ $# -gt 3 ];then
+echo "只接受三个数字参数"
+exit 3
+fi
+ 
+if [ $1 -gt $2 ];then
+ 
+MAX=$1
+MIN=$2
+ 
+else
+ 
+MAX=$2
+ 
+MIN=$1
+ 
+fi
+ 
+if [ $MIN -gt $3 ];then
+MIN=$3
+ 
+elif [ $MAX -lt $3 ];then
+ 
+MAX=$3
+ 
+fi
+ 
+echo "最大为$MAX,最小为$MIN"
